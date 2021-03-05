7b5d993544421bd16a9bc9959963c1015fed02b3
animesh <anamdeo@grepruby.io>
2021-03-05 13:24:06 +0530

    integrate bootstrap and implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def new
     @user = User.new
   end
@@ -7,7 +6,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.email.downcase!
-    
     if @user.save
       flash[:notice] = "Account created successfully!"
       redirect_to root_path
@@ -17,10 +15,8 @@ class UsersController < ApplicationController
     end
   end
 
-private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
-	root to: 'users#new'
-
-	get 'users/new' => 'users#new', as: :new_user
-	post 'users' => 'users#create'
+  root to: 'users#new'
+	#get 'users/new'  => 'users#new', as: :new_user
+	#post 'users'     => 'users#create'
+  resources :users
 	get '/login'     => 'sessions#new'
 	post '/login'    => 'sessions#create'
 	delete '/logout' => 'sessions#destroy'  
-  	
 end
