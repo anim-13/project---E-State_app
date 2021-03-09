ebc3df9497806d2f7b5325daed26d89d68c6ba71
anim-13 <78731420+anim-13@users.noreply.github.com>
2021-03-09 17:32:13 +0530

    User Authentication (#1)
    * User Authorization
    
    * added partial file
    
    * modified files
    
    * modified files
    
    * modified files and added some files
    
    * modified database.yml
    
    * modified files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  before_action :user_logged_in, if: :current_user, except: :destroy
+  
+  def new; end
+
+  def create
+    user = User.find_by(email: session_params[:email])  
+    if user&.authenticate(session_params[:password]) 
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Successfully logged in!'
+    else
+      flash.now.alert = "Incorrect email or password, try again."
+      render :new
+    end
+  end
+
+  def destroy  
+    session.delete(:user_id)
+    redirect_to login_path, notice: "Logged out!"
+  end
+
+  private
+  def session_params
+    params.require(:login).permit(:email, :password)
+  end
+
+  def user_logged_in
+    flash.new.alert = "Already logged in"
+    render :new
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  
+  has_secure_password
+  validates :email, presence: true, uniqueness: true	
+  validates :password, confirmation: true, length: {minimum: 6, maximum: 20}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+end
