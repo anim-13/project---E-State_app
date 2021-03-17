96d982ef75a9429fc89a3f94c07860d7aed3f37b
anim-13 <78731420+anim-13@users.noreply.github.com>
2021-03-17 11:15:06 +0530

    Manged User Profile (#4)
    * User Authorization
    
    * integrate bootstrap and implemented
    
    * added footer and logo in navigation bar
    
    * Adding footer and forms
    
    * added yarn file
    
    * pull code from user authentication
    
    * bootstrap implemented
    
    * Resolved conflict in application.html.erb
    
    * Modified Version
    
    * modified version
    
    * user profile version:1
    
    * User profile implemented
    
    * modified user controller
    
    * modified version
    
    * use before filter
    
    * changed method name
    
    * deleted .rake.tasks file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  
   def new
     @user = User.new
   end
@@ -14,8 +16,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    unless current_user.id == @user.id
+      flash.now.alert = "Denied Access!"
+      redirect_to login_path
+    end
+  end
+
+  def update
+    if @user.update(user_update)
+      redirect_to root_url, notice: "Profile updated Successfully"
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :contact)
   end  
+
+  def user_update
+    params.require(:user).permit(:name, :contact)
+  end
+
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_071719) do
+ActiveRecord::Schema.define(version: 2021_03_12_073342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_071719) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "contact"
   end
 
 end
