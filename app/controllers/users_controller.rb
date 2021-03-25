8f05b5652675a93909df5bb185944cb76cd86e21
animesh <anamdeo@grepruby.io>
2021-03-25 13:42:19 +0530

    Active Jobs implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(user: @user).welcome_email.deliver_later
+      UserJob.perform_later(@user)
       flash[:notice] = "Account created successfully!"
       redirect_to root_path
     else
