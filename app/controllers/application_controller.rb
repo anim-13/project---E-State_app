9d136e0ec8b35696a254bfaa148df40af0b78672
animesh <anamdeo@grepruby.io>
2021-03-31 13:48:36 +0530

    Devise Integration Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  helper_method :current_user
+  #protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :authenticate_user!
   
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :contact])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :contact])
   end
 end
