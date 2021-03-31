68d0de5d7f0b1d9bee63f98e18146aad42559104
anim-13 <78731420+anim-13@users.noreply.github.com>
2021-03-31 19:31:21 +0530

    Action Cable Implemented (#12)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    ActionCable.server.broadcast('notification_channel','You have visited Log in Page')
+  end
 
   # DELETE /resource/sign_out
   # def destroy
